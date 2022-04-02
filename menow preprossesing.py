6e437dc4c3d295621c83e45cdcde8006bf0975db
Marik Koltman <marikoltman2@gmail.com>
2022-04-02 19:13:50 +0300

    video sepperation version2

--- menow preprossesing.py
+++ menow preprossesing.py
@@ -1,21 +1,19 @@
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from keras.models import load_model
 import os
+import random
 from PIL import Image
 from mtcnn.mtcnn import MTCNN
 
 
-""" video sepperation version"""
+""" no video sepperation version"""
 
-# הבעיות עם השייפ - בגלל שזה אריי של ליסטים - לתקן בפונקציות
-
-
-#pip install git+https://github.com/keras-team/keras-preprocessing.git
 
 # create image label DataFrame
-def image_label_df(json_path, imgs_path):
+def image_label_df(json_path, imgs_path, imgs_per_person=5):
 
     # json to dataframe
     df = pd.read_json(json_path, orient='records')
@@ -25,8 +23,9 @@ def image_label_df(json_path, imgs_path):
     participants = os.listdir(imgs_path)
     col = []
     for i in range(len(participants)):
-        imgs = os.listdir(imgs_path + "\\" + participants[i])
-        col.append(imgs)
+        all_imgs = os.listdir(imgs_path + "\\" + participants[i])
+        rand_imgs = random.sample(all_imgs, imgs_per_person)
+        col.append(rand_imgs)
     d = {'participants': participants, 'imgs': col}
     imgs_df = pd.DataFrame(data=d)
     imgs_df.participants = imgs_df.participants.apply(lambda x: int(x))
@@ -42,11 +41,12 @@ def image_label_df(json_path, imgs_path):
     return img_label_df
 
 
+
 json_path = "C:\\Users\\USER1\\Desktop\\MeNow Project\\CC_annotations\\CasualConversations.json"
-imgs_path = "C:\\Users\\USER1\\Desktop\\MeNow Project\\Sampled Frames Dataset"
+imgs_path = "C:\\Users\\USER1\\Desktop\\MeNow Project\\part1"
 
-img_label_df = image_label_df(json_path, imgs_path)
-
+img_label_df = image_label_df(json_path, imgs_path, 10)
+print(img_label_df)
 
 
 ## Detect Faces
@@ -85,72 +85,52 @@ def extract_face(filename):
 
 
 # load images and extract faces for all images in a directory
-def load_faces(img_label_df, plot_example, num_face_per_video=5):
-    faces = list()    # (videos_list, img_list(10), 160 ,160 , 3)
-    participants = np.unique(img_label_df.index)
-    idx_les_5 = list()    # the indexs to drop from y - drop videos that not found 5 faces in them.
-    idx = 0
-    for par in participants:
-        par_df = img_label_df[img_label_df.index == par]
-        vid1_df = par_df[:100]
-        vid2_df = par_df[100:]
-        for vid_df in [vid1_df,vid2_df]:
-            vid_faces = list()
-            filename_list = list()
-            while len(vid_faces) < num_face_per_video:
-                filename = np.random.choice(vid_df.imgs.values, 1)[0]
-                if filename not in filename_list:
-                    img_path = imgs_path + "\\" + filename
-                    # get face
-                    face = extract_face(img_path)
-                    if face == "no face found":
-                        filename_list.append(filename)
-                        if len(filename_list) > (len(vid_faces) + 3):
-                            break        # give up this video
-                        else:
-                            continue     # keep trying find faces
-
-                    filename_list.append(filename)
-                    vid_faces.append(face)
-            if len(vid_faces) == 5:
-                faces.append(vid_faces)
-            else:
-                idx_les_5.append(idx)
-            idx += 1
-
+def load_faces(img_label_df, plot_example):
+    faces = list()
+    # enumerate files
+    for filename in img_label_df.imgs.values:
+        # path
+        img_path = imgs_path + "\\" + filename
+        # get face
+        face = extract_face(img_path)
+        # store
+        faces.append(face)
     if plot_example==True:
-        # display 10 faces from the extracted faces
+        # display 14 faces from the extracted faces
         i = 1
-        for face in faces[0]:
-            print(i, face.shape)
+        # enumerate files
+        for filename in img_label_df.imgs.values[:14]:
+            # path
+            img_path = imgs_path + "\\" + filename
+            # get face
+            face_arr = extract_face(img_path)
+            if face_arr == "no face found":
+                face_arr = np.zeros((160, 160, 3))
+            print(i, face_arr.shape)
+            # embedding = get_embedding(facenet_model, face)
+            # print(embedding.shape)
             # plot
-            plt.subplot(2, 5, i)
+            plt.subplot(2, 7, i)
             plt.axis('off')
-            plt.imshow(face)
+            plt.imshow(face_arr)
             i += 1
         plt.show()
-
-    return faces, idx_les_5
+    return faces
 
 
 # load a dataset that contains one subdir for each class that in turn contains images
 def load_dataset(img_label_df, plot_example=False):
-    X, idx_les_5 = load_faces(img_label_df, plot_example)
-    print(f"the indexs to drop from y: {idx_les_5}")
+    X = load_faces(img_label_df, plot_example)
     X = np.asarray(X)
+    print(f"X shape: {X.shape}")
     y = img_label_df.label.values
-    num_y_vid = X.shape[0] + len(idx_les_5)
-    y = np.reshape(y, (num_y_vid, -1))     # (videos_arr, labels(100))
-    if len(idx_les_5) > 0:
-        y = np.delete(y, np.asarray(idx_les_5), 0)
-    # Check all values in an array are equal to its first element
-    result = np.all(y[10] == y[10][0])
-    if result:
-        print('All Values in Array are same')
-    else:
-        print('All Values in Array are not same')
-    return X, y[:,:X.shape[1]]
-
+    # drop files were no face have found
+    idxs = np.where(X == "no face found")
+    idxs = idxs[0]
+    print(f"num of pic were no face found: {len(idxs)}")
+    X = np.delete(X, idxs, axis=(len(X.shape)-1))
+    y = np.delete(y, idxs)
+    return X, y
 
 # train test split
 def train_test_split(df,fruc):
@@ -161,23 +141,20 @@ def train_test_split(df,fruc):
     test_df = df[df.index > (num_parti*fruc)]
     return train_df, test_df
 
-train_df, test_df = train_test_split(img_label_df, 0.75)
+train_df, test_df = train_test_split(img_label_df, 0.7)
 print(f"train size: {len(train_df)}, test size: {len(test_df)}")
 
-
 # load train dataset
-trainX, trainy = load_dataset(train_df, plot_example=True)
-print(trainX.shape, trainy.shape)
+trainX, trainy = load_dataset(train_df, plot_example=False)
+print(trainX[0].shape, trainy.shape)
+print(trainX)
 # load test dataset
 testX, testy = load_dataset(test_df)
-print(f"final train size: {len(trainy)} videos, final test size: {len(testy)} videos")
+print(len(testX))
+print(f"final train size: {len(trainy)}, final test size: {len(testy)}")
 # save arrays to one file in compressed format
-np.savez_compressed('faces-dataset191.npz', trainX, trainy, testX, testy)
+np.savez_compressed('faces-dataset.npz', trainX, trainy, testX, testy)
 
-"""
-trainX, testX - is a 5 dimetional array (videos_arr, img_arr(5), 160 ,160 , 3)
-trainy, testy - is a 5 dimetional array (videos_arr, img_arr(5), 160 ,160 , 3)
-"""
 
 
 ## Create Face Embeddings
@@ -197,35 +174,28 @@ def get_embedding(model, face_pixels):
     return yhat[0]
 
 # load the face dataset
-data = np.load('faces-dataset191.npz', allow_pickle=True)
+data = np.load('faces-dataset.npz', allow_pickle=True)
 trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
 print('Loaded: ', trainX.shape, trainy.shape, testX.shape, testy.shape)
-
-
 # load the facenet model
 facenet_model = load_model('facenet_keras.h5')
 # summarize input and output shape
 print(facenet_model.inputs)
 print(facenet_model.outputs)
-
-
 # convert each face in the train set to an embedding
-def faces_to_embeddings(X_arr):
-    new_X_arr = list()
-    for vid_imgs in X_arr:
-        vid_embedding = list()
-        for face_pixels in vid_imgs:
-            embedding = get_embedding(facenet_model, face_pixels)
-            vid_embedding.append(embedding)
-        vid_embedding = np.asarray(vid_embedding)
-        new_X_arr.append(vid_embedding)
-    new_X_arr = np.asarray(new_X_arr)
-    return new_X_arr
-
-newTrainX = faces_to_embeddings(trainX)
+newTrainX = list()
+for face_pixels in trainX:
+    embedding = get_embedding(facenet_model, face_pixels)
+    newTrainX.append(embedding)
+newTrainX = np.asarray(newTrainX)
 print(newTrainX.shape)
-newTestX = faces_to_embeddings(testX)
+# convert each face in the test set to an embedding
+newTestX = list()
+for face_pixels in testX:
+    embedding = get_embedding(facenet_model, face_pixels)
+    newTestX.append(embedding)
+newTestX = np.asarray(newTestX)
 print(newTestX.shape)
-
 # save arrays to one file in compressed format
-np.savez_compressed('faces-embeddings191.npz', newTrainX, trainy, newTestX, testy)
+np.savez_compressed('faces-embeddings.npz', newTrainX, trainy, newTestX, testy)
+
