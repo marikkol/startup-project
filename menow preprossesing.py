6e536c9a5ef1e1d6ecb10a1ab29d52dcd22cb1eb
Marik Koltman <marikoltman2@gmail.com>
2022-04-02 16:26:22 +0300

    first commit

---
+++ menow preprossesing.py
@@ -0,0 +1,306 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from keras.models import load_model
+import os
+import random
+from PIL import Image
+from mtcnn.mtcnn import MTCNN
+
+""" no video sepperation version"""
+
+#pip install git+https://github.com/keras-team/keras-preprocessing.git
+
+# create image label DataFrame
+def image_label_df(json_path, imgs_path, imgs_per_person=5):
+
+    # json to dataframe
+    df = pd.read_json(json_path, orient='records')
+    df = df.T
+
+    # create df containing the participants index to the images in a string column
+    participants = os.listdir(imgs_path)
+    col = []
+    for i in range(len(participants)):
+        all_imgs = os.listdir(imgs_path + "\\" + participants[i])
+        rand_imgs = random.sample(all_imgs, imgs_per_person)
+        col.append(rand_imgs)
+    d = {'participants': participants, 'imgs': col}
+    imgs_df = pd.DataFrame(data=d)
+    imgs_df.participants = imgs_df.participants.apply(lambda x: int(x))
+    imgs_df = imgs_df.set_index('participants')
+
+    # new df of filename col and skin-type(label) col
+    img_label_df = pd.concat([df, imgs_df], axis=1, join="inner")
+    img_label_df = img_label_df.explode('imgs')
+    #img_label_df.label = img_label_df.label.apply(lambda x: x['skin-type'])     להכליל את הלייבלים, לשימוש של שלושתם
+    img_label_df.imgs = np.array([str(x) + "\\" for x in img_label_df.index]) + img_label_df.imgs
+    img_label_df = img_label_df[['imgs', 'label']]
+
+    return img_label_df
+
+
+
+json_path = "C:\\Users\\USER1\\Desktop\\MeNow Project\\CC_annotations\\CasualConversations.json"
+imgs_path = "C:\\Users\\USER1\\Desktop\\MeNow Project\\part1"
+
+img_label_df = image_label_df(json_path, imgs_path, 10)
+print(img_label_df)
+
+
+## Detect Faces
+
+
+# extract a single face from a given photograph
+def extract_face(filename):
+    required_size = (160, 160)
+    # load image from file
+    image = Image.open(filename)
+    # convert to RGB, if needed
+    image = image.convert('RGB')
+    # convert to array
+    pixels = np.asarray(image)
+    # create the detector, using default weights
+    detector = MTCNN()
+    # detect faces in the image
+    results = detector.detect_faces(pixels)
+    print(filename)
+    if len(results)==0:
+        print("no face found")
+        return "no face found"
+    # extract the bounding box from the first face
+    print(results)
+    x1, y1, width, height = results[0]['box']
+    # bug fix
+    x1, y1 = abs(x1), abs(y1)
+    x2, y2 = x1 + width, y1 + height
+    # extract the face
+    face = pixels[y1:y2, x1:x2]
+    # resize pixels to the model size
+    image = Image.fromarray(face)
+    image = image.resize(required_size)
+    face_array = np.asarray(image)
+    return face_array
+
+
+# load images and extract faces for all images in a directory
+def load_faces(img_label_df, plot_example):
+    faces = list()
+    # enumerate files
+    for filename in img_label_df.imgs.values:
+        # path
+        img_path = imgs_path + "\\" + filename
+        # get face
+        face = extract_face(img_path)
+        # store
+        faces.append(face)
+    if plot_example==True:
+        # display 14 faces from the extracted faces
+        i = 1
+        # enumerate files
+        for filename in img_label_df.imgs.values[:14]:
+            # path
+            img_path = imgs_path + "\\" + filename
+            # get face
+            face_arr = extract_face(img_path)
+            if face_arr == "no face found":
+                face_arr = np.zeros((160, 160, 3))
+            print(i, face_arr.shape)
+            # embedding = get_embedding(facenet_model, face)
+            # print(embedding.shape)
+            # plot
+            plt.subplot(2, 7, i)
+            plt.axis('off')
+            plt.imshow(face_arr)
+            i += 1
+        plt.show()
+    return faces
+
+
+# load a dataset that contains one subdir for each class that in turn contains images
+def load_dataset(img_label_df, plot_example=False):
+    X = load_faces(img_label_df, plot_example)
+    X = np.asarray(X)
+    print(f"X shape: {X.shape}")
+    y = img_label_df.label.values
+    # drop files were no face have found
+    idxs = np.where(X == "no face found")
+    idxs = idxs[0]
+    print(f"num of pic were no face found: {len(idxs)}")
+    X = np.delete(X, idxs, axis=(len(X.shape)-1))
+    y = np.delete(y, idxs)
+    return X, y
+
+# train test split
+def train_test_split(df,fruc):
+    participants = np.unique(df.index)
+    num_parti = len(participants)
+    print(f"the num of participants: {num_parti}")
+    train_df = df[df.index < (num_parti*fruc)]
+    test_df = df[df.index > (num_parti*fruc)]
+    return train_df, test_df
+
+train_df, test_df = train_test_split(img_label_df, 0.7)
+print(f"train size: {len(train_df)}, test size: {len(test_df)}")
+
+# load train dataset
+trainX, trainy = load_dataset(train_df, plot_example=False)
+print(trainX[0].shape, trainy.shape)
+print(trainX)
+# load test dataset
+testX, testy = load_dataset(test_df)
+print(len(testX))
+print(f"final train size: {len(trainy)}, final test size: {len(testy)}")
+# save arrays to one file in compressed format
+np.savez_compressed('faces-dataset.npz', trainX, trainy, testX, testy)
+
+
+
+## Create Face Embeddings
+
+
+# get the face embedding for one face
+def get_embedding(model, face_pixels):
+    # scale pixel values
+    face_pixels = face_pixels.astype('float32')
+    # standardize pixel values across channels (global)
+    mean, std = face_pixels.mean(), face_pixels.std()
+    face_pixels = (face_pixels - mean) / std
+    # transform face into one sample
+    samples = np.expand_dims(face_pixels, axis=0)
+    # make prediction to get embedding
+    yhat = model.predict(samples)
+    return yhat[0]
+
+# load the face dataset
+data = np.load('faces-dataset.npz', allow_pickle=True)
+trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
+print('Loaded: ', trainX.shape, trainy.shape, testX.shape, testy.shape)
+# load the facenet model
+facenet_model = load_model('facenet_keras.h5')
+# summarize input and output shape
+print(facenet_model.inputs)
+print(facenet_model.outputs)
+# convert each face in the train set to an embedding
+newTrainX = list()
+for face_pixels in trainX:
+    embedding = get_embedding(facenet_model, face_pixels)
+    newTrainX.append(embedding)
+newTrainX = np.asarray(newTrainX)
+print(newTrainX.shape)
+# convert each face in the test set to an embedding
+newTestX = list()
+for face_pixels in testX:
+    embedding = get_embedding(facenet_model, face_pixels)
+    newTestX.append(embedding)
+newTestX = np.asarray(newTestX)
+print(newTestX.shape)
+# save arrays to one file in compressed format
+np.savez_compressed('faces-embeddings.npz', newTrainX, trainy, newTestX, testy)
+
+
+
+
+
+"""
+
+
+
+# initialize Keras’ ImageDataGenerator class
+train_datagen = ImageDataGenerator(rescale=1/ 255.0, validation_split=0.20)
+test_datagen = ImageDataGenerator(rescale=1 / 255.0)
+
+# initialize our training, validation and testing generator
+batch_size = 8
+train_generator = train_datagen.flow_from_dataframe(
+    dataframe=train_df,
+    directory=path,
+    x_col="imgs",
+    y_col="label",
+    target_size=(100, 100),
+    batch_size=batch_size,
+    class_mode="categorical",
+    subset='training',
+    shuffle=True,
+    seed=42
+)
+valid_generator = train_datagen.flow_from_dataframe(
+    dataframe=train_df,
+    directory=path,
+    x_col="imgs",
+    y_col="label",
+    target_size=(100, 100),
+    batch_size=batch_size,
+    class_mode="categorical",
+    subset='validation',
+    shuffle=True,
+    seed=42
+)
+test_generator = test_datagen.flow_from_dataframe(
+    dataframe=test_df,
+    directory=path,
+    x_col="imgs",
+    target_size=(100, 100),
+    batch_size=1,
+    class_mode=None,
+    shuffle=False,
+)
+
+#
+# #define the metrics
+# METRICS = [
+#       keras.metrics.TruePositives(name='tp'),
+#       keras.metrics.FalsePositives(name='fp'),
+#       keras.metrics.TrueNegatives(name='tn'),
+#       keras.metrics.FalseNegatives(name='fn'),
+#       keras.metrics.BinaryAccuracy(name='accuracy'),
+#       keras.metrics.Precision(name='precision'),
+#       keras.metrics.Recall(name='recall'),
+#       keras.metrics.AUC(name='auc'),
+#       keras.metrics.AUC(name='prc', curve='PR'), # precision-recall curve
+# ]
+#
+#
+# # Function that creates a simple neural network
+# def make_model(metrics=METRICS):
+#     input = tf.keras.Input(shape=(32,))
+#     face = extract_face(input)
+#     embedding = get_embedding(facenet_model, face)
+#     # x = keras.layers.Dense(32, activation="relu")(embedding)
+#     # x = keras.layers.Dropout(0.5)(x)
+#     # x = keras.layers.Dense(1)(x)
+#     # output = tf.nn.sigmoid(x)
+#     output = embedding
+#
+#     model = keras.Model(input, output)
+#     print(model.summary())
+#     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
+#                   loss=tf.keras.losses.BinaryCrossentropy(),
+#                   metrics=METRICS)
+#     return model
+#
+# make_model()
+
+
+#
+# print(tf.config.list_physical_devices())
+# print(tf.test.is_gpu_available())
+
+
+
+
+
+# df['all_files'] = df[['dark_files', 'files']].sum(axis=1)
+# print(len(df.dark_files[1]) + len(df.files[1]) == len(df.all_files[1]))
+# df = df.explode('all_files')
+# df_label = df.label.apply(lambda x: int(x['skin-type'])).reset_index(drop=True)
+# df_filename = df.all_files.reset_index(drop=True)
+# df_skintype = pd.concat([df_filename, df_label], axis=1).rename(columns={'all_files': 'filename'})
+# print(df_skintype)
+
+
+"""
+
+
+
---
+++ menow single-frame model.py
@@ -0,0 +1,259 @@
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import os
+import tempfile
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+import sklearn
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
+
+
+
+"""
+single-frame model
+"""
+
+
+# load dataset
+data = np.load('faces-embeddings191.npz', allow_pickle=True)
+trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
+print('Dataset: train=%d, test=%d' % (trainX.shape[0], testX.shape[0]))
+
+"""
+X shape:   (vids, imgs, embeddings)
+y shape:   (vidslable, imgs)
+"""
+
+print(trainX.shape, trainy.shape, testX.shape, testy.shape)
+
+
+
+# # convert to object oriented (class single_frame_model)
+# def single_frame_model(trainX, trainy, testX, testy, lable):
+
+
+
+# Examine the class label imbalance
+def show_lable_dist(labels_arr, label):
+    labels_arr = labels_arr[:, 0]
+    if label == "skin-type":
+        label_arr = np.array([int(xi[label]) for xi in labels_arr])
+        unique = np.unique(label_arr)
+        bincount = np.bincount(label_arr)
+        total = bincount.sum()
+        print('Examples:')
+        print(f'    Total: {total}')
+        for label in unique:
+            print(f'    type' + str(label) + f': {bincount[label]} ({(100 * bincount[label]/total):.2f} of total)')
+    # if label == "gender":
+    # if label == "age":
+    return total
+
+print(show_lable_dist(trainy, "skin-type"))
+
+
+# Split the data into training, validation, and test sets
+trainX, valX, trainy, valy= train_test_split(trainX, trainy, test_size=0.2)
+
+print(len(trainX), 'training examples')
+print(len(valX), 'validation examples')
+print(len(testX), 'test examples')
+
+
+
+# Create an input pipeline using tf.data
+
+batch_size = 32
+
+def arr_to_dataset(Xarr, yarr, label='skin-type', model_type='single-frame',  shuffle=True, batch_size=32):
+    if model_type == 'single-frame':
+        Xarr = Xarr.reshape([-1, 128])
+        yarr = yarr.reshape([-1, 1])
+        yarr = np.array([int(xi[label]) for xi in yarr[:,0]])
+        print(f'Xarr shape: {Xarr.shape}, yarr shape: {yarr.shape}')
+    elif model_type == 'late-fusion':
+        yarr = yarr[:,0]
+    ds = tf.data.Dataset.from_tensor_slices((Xarr, yarr))
+    if shuffle:
+        ds = ds.shuffle(buffer_size=len(Xarr))
+    ds = ds.batch(batch_size)
+    ds = ds.prefetch(batch_size)
+    return ds
+
+# Checking the format of the data the df_to_dataset function returns
+train_ds = arr_to_dataset(trainX, trainy)
+[(embedding_batch, label_batch)] = train_ds.take(1)
+print('embedding batch shape:', embedding_batch.shape)
+print('label batch shape:', label_batch.shape)
+
+
+
+# # Apply the Keras preprocessing layers
+# # perform feature-wise normalization of input features.
+#
+# def get_normalization_layer(vid_num, dataset):
+#     normalizer = layers.Normalization(axis=None)
+#     feature_ds = dataset.map(lambda x, y: x[vid_num])
+#     normalizer.adapt(feature_ds)
+#     return normalizer
+#
+# # Test the get_normalization_layer function
+# vid_1 = train_features['age']
+# layer = get_normalization_layer('age', train_ds)
+# layer(age_col)
+
+
+batch_size = 32
+train_ds = arr_to_dataset(trainX, trainy)
+val_ds = arr_to_dataset(valX, valy, shuffle=False, batch_size=batch_size)
+test_ds = arr_to_dataset(testX, testy, shuffle=False, batch_size=batch_size)
+
+
+
+# Create, compile, and train the model
+
+
+#define the metrics
+METRICS = [
+      tf.keras.metrics.TruePositives(name='tp'),
+      tf.keras.metrics.FalsePositives(name='fp'),
+      tf.keras.metrics.TrueNegatives(name='tn'),
+      tf.keras.metrics.FalseNegatives(name='fn'),
+      tf.keras.metrics.BinaryAccuracy(name='accuracy'),
+      tf.keras.metrics.Precision(name='precision'),
+      tf.keras.metrics.Recall(name='recall'),
+      tf.keras.metrics.AUC(name='auc'),
+      tf.keras.metrics.AUC(name='prc', curve='PR'), # precision-recall curve
+]
+
+# Function that creates a simple neural network
+def make_model(label="skin-type", model_type='single-frame', metrics=['accuracy']):
+    label_arr = np.array([int(xi[label]) for xi in testy[:,0]])
+    unique = np.unique(label_arr)
+    num_labels = len(unique)
+    print(num_labels)
+
+    if model_type == 'single-frame':
+        embedding_input = tf.keras.Input(shape=(128,), name='embedding_input')
+        x = tf.keras.layers.Dense(32, activation="relu")(embedding_input)
+        x = tf.keras.layers.Dropout(0.5)(x)
+        x = tf.keras.layers.Dense(num_labels+1)(x)
+        output = tf.nn.sigmoid(x)
+        model = tf.keras.Model(embedding_input, output)
+
+    # elif model_type == 'late-fusion':
+    #     # merge the list of feature inputs
+    #     all_features = tf.keras.layers.concatenate(encoded_features)
+    #     x = tf.keras.layers.Dense(32, activation="relu")(all_features)
+    #     x = tf.keras.layers.Dropout(0.5)(x)
+    #     x = tf.keras.layers.Dense(1)(x)
+    #     output = tf.nn.sigmoid(x)
+
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                  metrics=metrics)
+
+    return model
+
+
+# Model summary
+model = make_model()
+print(model.summary())
+
+
+# train the model
+EPOCHS = 60
+
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor='val_prc',
+    verbose=1,
+    patience=10,
+    mode='max',
+    restore_best_weights=True)
+
+history = model.fit(
+    train_ds,
+    epochs=EPOCHS,
+    callbacks=[early_stopping],
+    validation_data=val_ds)
+
+
+# Produce plots of the model's loss on the training and validation set (useful to check for overfitting),
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(EPOCHS)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+# create an evaluation function to output all the needs metrics
+
+def evaluate_preds(y_true, y_preds):
+    """
+    Performs evaluation comparison on y_true labels vs. y_pred labels
+    on a classification.
+    """
+    accuracy = accuracy_score(y_true, y_preds)
+    precision = precision_score(y_true, y_preds, average='micro')
+    recall = recall_score(y_true, y_preds, average='micro')
+    f1 = f1_score(y_true, y_preds, average='micro')
+    metric_dict = {"accuracy": round(accuracy, 2),
+                   "precision": round(precision, 2),
+                   "recall": round(recall, 2),
+                   "f1": round(f1, 2)}
+    print(f"Acc: {accuracy * 100:.2f}%")
+    print(f"Precision: {precision:.2f}")
+    print(f"Recall: {recall:.2f}")
+    print(f"F1 score: {f1:.2f}")
+
+    return metric_dict
+
+# Now we make predictions using the test data to see how the model performs
+pred_y = model.predict(test_ds)
+print(pred_y.shape, testy.shape)
+testy = testy.reshape([-1, 1])
+testy = np.array([int(xi['skin-type']) for xi in testy[:, 0]])
+print(pred_y.shape, testy.shape)
+evaluate_preds(testy, np.argmax(pred_y, axis=1))
+
+
+# Create a Classification Report
+print(classification_report(testy, np.argmax(pred_y, axis=1)))
+
+conf_mat = confusion_matrix(testy, np.argmax(pred_y, axis=1))
+fig, ax = plt.subplots(figsize=(8,6))
+sns.heatmap(conf_mat, annot=True, fmt='d')
+plt.ylabel('Actual')
+plt.xlabel('Predicted')
+plt.show()
+
+
+
+
+
+
